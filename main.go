d65c4d99524d7427c251b5c2f466b8a3c4cce4ad
Labyrintos <Labyrintos71@gmail.com>
2020-07-26 16:58:16 +0900

    time api 파일 제거

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 	"tradingview_udf_binance_go/api"
 
 	"github.com/labstack/echo"
@@ -14,10 +16,16 @@ func main() {
 	//미들웨어 선언
 	e.Use(middleware.Logger())  //http 요청 기록
 	e.Use(middleware.Recover()) //패닉 복구
-	e.GET("/time", api.GetTime)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
+
+	e.GET("/time", func(c echo.Context) error {
+		return c.String(http.StatusOK, strconv.FormatInt(time.Now().Unix(), 10))
+	})
+
+	e.GET("/config", api.GetConfig)
+	e.GET("/symbols", api.Symbols)
 	// http://localhost:8081/
 	e.Logger.Fatal(e.Start(":8081"))
 
@@ -25,14 +33,12 @@ func main() {
 
 /*
 ```js
-GET /config
 GET /symbol_info?group=<group_name>
 GET /symbols?symbol=<symbol>
 GET /search?query=<query>&type=<type>&exchange=<exchange>&limit=<limit>
 GET /history?symbol=<ticker_name>&from=<unix_timestamp>&to=<unix_timestamp>&resolution=<resolution>
 GET /marks?symbol=<ticker_name>&from=<unix_timestamp>&to=<unix_timestamp>&resolution=<resolution>
 GET /timescale_marks?symbol=<ticker_name>&from=<unix_timestamp>&to=<unix_timestamp>&resolution=<resolution>
-GET /time
 GET /quotes?symbols=<ticker_name_1>,<ticker_name_2>,...,<ticker_name_n>
 ```
 */
